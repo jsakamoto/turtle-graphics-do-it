9e2f861b458cdcce3884baf7537e01dbafc73515
jsakamoto <junichi.jp@gmail.com>
2013-09-30 00:35:48 +0900

    1st commit.

---
+++ turtle-graphics.js
@@ -0,0 +1,24 @@
+﻿(function (exports) {
+    var canvas = window.document.getElementsByTagName('canvas')[0];
+    var ctx = canvas.getContext('2d');
+    var width = parseInt(canvas.width);
+    var heigt = parseInt(canvas.height);
+
+    // default state.
+    var x = width / 2.0;
+    var y = heigt / 2.0;
+    var dir = 0.0;
+    var isPenDown = true;
+
+    var moveTo = function (distance) {
+        var fn = isPenDown ? ctx.lineTo : ctx.moveTo;
+        ctx.moveTo(x, y);
+
+    };
+
+    var turtle = {
+        'moveTo': moveTo
+    };
+    exports.turtle = turtle;
+
+})(window);
