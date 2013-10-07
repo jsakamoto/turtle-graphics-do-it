619cb97c5277e77496b841d1f3db4bfb3a0e538d
jsakamoto <junichi.jp@gmail.com>
2013-10-07 23:31:02 +0900

    Remove ASP.NEt WebAPI Components.

--- TurtleGraphicsDoIt/Global.asax.cs
+++ TurtleGraphicsDoIt/Global.asax.cs
@@ -19,7 +19,6 @@ namespace TurtleGraphicsDoIt
 
             AreaRegistration.RegisterAllAreas();
 
-            //WebApiConfig.Register(GlobalConfiguration.Configuration);
             //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
